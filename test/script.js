0e6765aef48e72514103e52867fdac6d096285db
Sujay Singh <38438205+cvenomist@users.noreply.github.com>
2018-12-21 18:00:50 +0530

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,32 @@
+let x = 'let variable';
+
+var y = 'var variable';
+
+function test() {
+    //console.log(x);
+    //console.log(y);
+}
+
+function demo() {
+
+    a = 'variable without inside demo()';
+    var b = 'var variable inside demo()';
+
+}
+
+demo();
+var b;
+//console.log(a);
+//console.log(b);
+
+//test();
+
+if(1) {
+
+    var i = 'var variable inside if()';
+    j = 'let variable inside if()';
+
+}
+
+//console.log(i);
+//console.log(j);
